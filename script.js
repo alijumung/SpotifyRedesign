2250c54e8569050df6fb7bc47aba0119fdacc43e
Just A Jumung <110404415+alijumung@users.noreply.github.com>
2022-10-11 14:17:44 +0300

    redesigning spotify login

---
+++ script.js
@@ -0,0 +1,8 @@
+function yoo() {
+    var x = document.getElementById("sup");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
